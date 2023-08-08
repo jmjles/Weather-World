ecf4057d44b5d1103c743b523ef3dd6b99ee268d
jmjles <jmjles@gmail.com>
2023-08-07 17:11:53 -0700

    Devop: added forcast section

--- src/components/forcast/Forcast.tsx
+++ src/components/forcast/Forcast.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Grid } from "@mui/material";
+import { Day } from "../../types";
+import WeatherCard from "../weatherCard/WeatherCard.tsx";
 
-const Forcast = () => {
+const Forcast = ({ Days }: props) => {
   return (
-    <div>Forcast</div>
-  )
-}
-
-export default Forcast
+    <Grid container>
+      {Days.map((day) => (
+        <Grid item>
+          <WeatherCard {...day} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+type props = {
+  Days: Day[];
+};
+export default Forcast;

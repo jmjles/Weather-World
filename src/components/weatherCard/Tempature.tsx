0000dd78eb55a0f334ada090865ee8701a82a32a
jmjles <jmjles@gmail.com>
2023-08-07 17:10:19 -0700

    Devop: added tempature template

---
+++ src/components/weatherCard/Tempature.tsx
@@ -0,0 +1,27 @@
+import { Grid, Typography as Font } from "@mui/material";
+
+const Tempature = ({ title, image, alt, content }: props) => {
+  return (
+    <Grid container direction="column" alignItems="center">
+      <Grid item>
+        {image ? (
+          <img src={image} alt={alt} />
+        ) : (
+          <Font variant="h5">{title}</Font>
+        )}
+      </Grid>
+      <Grid item>
+        <Font variant="subtitle1" fontWeight="Bold">
+          {content}
+        </Font>
+      </Grid>
+    </Grid>
+  );
+};
+type props = {
+  title?: string;
+  image?: string;
+  alt?: string;
+  content: string;
+};
+export default Tempature;

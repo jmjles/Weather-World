ca338b6269fd90ca3f8eb6bf4a58c00dd2ba4937
jmjles <jmjles@gmail.com>
2023-08-08 15:49:51 -0700

    devop: added search functionality

---
+++ src/components/search/Search.tsx
@@ -0,0 +1,63 @@
+import { Autocomplete, Grid, TextField } from "@mui/material";
+import { Location } from "../../types";
+
+const Search = ({
+  handleChange,
+  handleSubmit,
+  locations,
+  handleLocation,
+  query,
+  selected,
+}: props) => {
+  return (
+    <div>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <Grid container alignItems="center" spacing={2}>
+          <Grid item>
+            <Autocomplete
+              fullWidth
+              disableClearable
+              options={locations}
+              getOptionLabel={(l: Location) => {
+                return l.state
+                  ? `${l.name}, ${l.state}, ${l.country}`
+                  : `${l.name}, ${l.country}`;
+              }}
+              isOptionEqualToValue={(option, val) =>
+                option.lat === val.lat && option.lon === val.lon
+              }
+              onChange={(e, val: Location) => handleLocation(val)}
+              value={selected}
+              inputValue={query}
+              onInputChange={(e, val) => {
+                handleChange(val);
+              }}
+              sx={{ minWidth: 325 }}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  placeholder="Enter City, Zipcode, or State"
+                  fullWidth
+                  label="Search"
+                  InputProps={{
+                    ...params.InputProps,
+                    type: "search",
+                  }}
+                />
+              )}
+            />
+          </Grid>
+        </Grid>
+      </form>
+    </div>
+  );
+};
+type props = {
+  handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+  handleChange: (val: string) => void;
+  locations: Location[];
+  handleLocation: React.Dispatch<React.SetStateAction<Location>>;
+  query: string;
+  selected: Location;
+};
+export default Search;

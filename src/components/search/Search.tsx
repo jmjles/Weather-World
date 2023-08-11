109eb00eabef80fe07509650bdffe51f8c7e4978
jmjles <jmjles@gmail.com>
2023-08-11 14:33:07 -0700

    Devop: improved error catching, added quality of life changes

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,6 +1,7 @@
 import { Autocomplete, Grid, TextField } from "@mui/material";
-import { Location, Search as props } from "../../types";
-
+import { Location, Search as SearchProps } from "../../types";
+import { getLocationName } from "../utils";
+import { useState } from "react";
 const Search = ({
   handleChange,
   handleSubmit,
@@ -8,7 +9,17 @@ const Search = ({
   handleLocation,
   query,
   selected,
+  loading,
+  fetchError,
 }: props) => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    if(!locations.length) setOpen(false)
+    else {
+      setOpen((prev) => !prev);
+    }
+  };
   return (
     <div>
       <form onSubmit={(e) => handleSubmit(e)}>
@@ -19,16 +30,19 @@ const Search = ({
               disableClearable
               options={locations}
               getOptionLabel={(l: Location) => {
-                return l.state
-                  ? `${l.name}, ${l.state}, ${l.country}`
-                  : `${l.name}, ${l.country}`;
+                return getLocationName(l.name, l.state, l.country);
               }}
               isOptionEqualToValue={(option, val) =>
                 option.lat === val.lat && option.lon === val.lon
               }
-              onChange={(e, val: Location) => handleLocation(val)}
-              value={selected}
+              onChange={(e, val: Location) => handleLocation(val ? val : null)}
               inputValue={query}
+              value={selected}
+              open={open}
+              onClose={handleOpen}
+              onOpen={handleOpen}
+              noOptionsText="Hit enter to search"
+              loading={loading}
               onInputChange={(e, val) => {
                 handleChange(val);
               }}
@@ -36,8 +50,11 @@ const Search = ({
               renderInput={(params) => (
                 <TextField
                   {...params}
-                  placeholder="Enter City, Zipcode, or State"
+                  placeholder="Enter a city name"
                   fullWidth
+                  error={fetchError.length ? true : false}
+                  helperText={fetchError}
+                  required
                   label="Search"
                   InputProps={{
                     ...params.InputProps,
@@ -52,4 +69,8 @@ const Search = ({
     </div>
   );
 };
+interface props extends SearchProps {
+  loading: boolean;
+  fetchError: string;
+}
 export default Search;

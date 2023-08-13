eea904f47d1a9451d527b2f3958c3238160dce21
jmjles <jmjles@gmail.com>
2023-08-12 17:27:17 -0700

    Devop: moved theme to new file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
-
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { theme } from "./theme.ts";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const theme = createTheme({
-  typography: {
-    h1: { fontSize: 64 },
-    h2: { fontSize: 36 },
-    subtitle1: { fontSize: 12 },
-  },
-});
 
 root.render(
   <React.StrictMode>
